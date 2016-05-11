17a157c2a9aa6c86cab0c538a907747578ab8b12
sudasf <1518223884@qq.com>
2016-05-11 11:15:54 +0800

    change

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var http=require('http')
 var server=http.createServer(function(req,res){
 	res.writeHead(200,{'Content-Type':'text/plain;charset=utf8'})
-	res.end('这是孙峰的阿里云服务器')
+	res.end('这是孙峰的阿里云服务器 hahahah')
 })
 server.listen(3000)
