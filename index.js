2d07b8df8e7c87e90a47e147b582f1d7d11c70c2
sudasf <1518223884@qq.com>
2016-05-11 14:32:55 +0800

    banana

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var http=require('http')
+var fs=require('fs')
 var server=http.createServer(function(req,res){
-	res.writeHead(200,{'Content-Type':'text/plain;charset=utf8'})
-	res.end('这是孙峰的阿里云服务器 shabi')
+	res.writeHead(200,{'Content-Type':'text/html;charset=utf8'})
+	fs.readFile('./index.html',function(err,data){
+		res.end(data)
+	})
+	
 })
 server.listen(3000)
 
